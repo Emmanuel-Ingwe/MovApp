499aabd81e35d6913b0937f1c346789669f3535a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-17 14:54:46 +0200

    APIs

--- movie.js
+++ movie.js
@@ -1,12 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'd9835cc5',
-            i: 'tt0848228'
+            s: searchTerm
         }
     });
 
     console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+});
