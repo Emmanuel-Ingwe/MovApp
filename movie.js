cc34226d76425feefdca87e809e808a8029275aa
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-18 07:52:50 +0200

    Apiii

--- movie.js
+++ movie.js
@@ -6,13 +6,28 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.search;
 };
 
 const input = document.querySelector('input');
 
 
-const onInput = event => {
-    fetchData(event.target.value);
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    for (let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+        <img src="${movie.Poster}"/>
+        <h1>${movie.Title}<h/>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 500));
